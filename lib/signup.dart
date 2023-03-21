ab1e0bfba9cbb585975e6fa9a8b2ead4ce4bd0a6
Yekto Priyandhani <yekto.priyandhani@anabatic.com>
2023-03-21 19:30:40 +0700

    validator email
    String p = r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
    
      RegExp regExp = new RegExp(p);

--- lib/signup.dart
+++ lib/signup.dart
@@ -6,6 +6,9 @@ class SignUp extends StatefulWidget {
 }
 
 final _formKey = GlobalKey<FormState>();
+String p =
+    r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+RegExp regExp = new RegExp(p);
 
 class _SignUpState extends State<SignUp> {
   @override
@@ -43,15 +46,15 @@ class _SignUpState extends State<SignUp> {
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: <Widget>[
-                      TextFormField(validator:(value){
-                        if (value!.length < 6){
-                          return "Username is too Short";
-                        }
-                        else if (value==""){
-                          return "Please fill this form";
-                        }
-                        return"";
-                      },
+                      TextFormField(
+                        validator: (value) {
+                          if (value!.length < 6) {
+                            return "Username is too Short";
+                          } else if (value == "") {
+                            return "Please fill this form";
+                          }
+                          return "";
+                        },
                         decoration: InputDecoration(
                           hintText: "UserName",
                           hintStyle: TextStyle(color: Colors.black),
@@ -59,6 +62,14 @@ class _SignUpState extends State<SignUp> {
                         ),
                       ),
                       TextFormField(
+                        validator: (value) {
+                          if (value == "") {
+                            return "Please fill this form Dude";
+                          } else if (!regExp.hasMatch(value!)) {
+                            return "Email invalid Dude";
+                          }
+                          return "";
+                        },
                         decoration: InputDecoration(
                           hintText: "Email",
                           hintStyle: TextStyle(color: Colors.black),
@@ -102,9 +113,7 @@ class _SignUpState extends State<SignUp> {
                       Row(
                         children: <Widget>[
                           Text("I have already an Account Dude"),
-                          SizedBox(
-                              width: 5
-                          ),
+                          SizedBox(width: 5),
                           GestureDetector(
                             child: Text(
                               "Login",
